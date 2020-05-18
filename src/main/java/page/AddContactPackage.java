ad08f9339e4912e2994c092b1fc85bcb79a2e588
bmohaim <benozir15@gmail.com>
2020-05-18 01:20:45 -0500

    3rd commit

--- src/main/java/page/AddContactPackage.java
+++ src/main/java/page/AddContactPackage.java
@@ -1,6 +1,9 @@
 package page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
 public class AddContactPackage {
 WebDriver driver;
@@ -9,4 +12,8 @@ WebDriver driver;
 		this.driver = driver;
 	}
 
+	
+	@FindBy(how = How.ID, using = "username") WebElement USERNAME_FIELD_LOCATOR;
+	@FindBy(how = How.ID, using = "password") WebElement PASSWORD_FIELD_LOCATOR;
+	@FindBy(how = How.NAME, using = "login") WebElement SIGNIN_BUTTON_LOCATOR;
 }
